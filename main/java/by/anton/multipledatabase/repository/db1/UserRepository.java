0e0e0e533398882b9c71944a33508e0a83b21681
antonbl32 <60536119+antonbl32@users.noreply.github.com>
2021-07-05 16:51:58 +0300

    Delete main directory

--- main/java/by/anton/multipledatabase/repository/db1/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package by.anton.multipledatabase.repository.db1;
-
-import by.anton.multipledatabase.entity.db1.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
--- main/java/by/anton/multipledatabase/repository/db2/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package by.anton.multipledatabase.repository.db2;
-
-import by.anton.multipledatabase.entity.db2.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartmentRepository extends JpaRepository<Department, Integer> {
-}
